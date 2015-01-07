2df189860a4d2cda6d80531317997ae72430037c
Hadi <hadichalabi@gmail.com>
2015-01-07 17:05:38 +0000

    generated photo model with name and rating properties

--- spec/features/photo_feature_spec.rb
+++ spec/features/photo_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'photos' do
       expect(page).to have_link 'Add a photo'
     end
   end
+
+  context 'photos have been added' do
+    before do
+      Photo.create(name: 'Holiday in Turkey')
+    end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('Holiday in Turkey')
+      expect(page).not_to have_content('No photos yet')
+    end
+  end
 end
